fb639a1ba1d84302013f68b6ad743190b6215022
luka.verbrugghe <lotrdetailsys@gmail.com>
2024-09-12 11:30:13 +0200

    Sofo

--- Form1.cs
+++ Form1.cs
@@ -32,17 +32,47 @@ namespace _001Inleiding
             SolidBrush redBrush = new SolidBrush(Color.Red);
 
             //pen nodig om te kunnen tekenen
-            Pen redPen = new Pen(redBrush, 5);
+            Pen redPen = new Pen(redBrush, 2);
 
             //teken cirkel
             g.DrawEllipse(redPen, 50, 100, 100, 100);
 
             //opgevulde rechthoek (geel)
-            g.FillRectangle(new SolidBrush(Color.Yellow), 50, 150, 100, 200);
+            g.FillRectangle(new SolidBrush(Color.Yellow), 150, 150, 100, 200);
 
             //driehoek (3 delige poligon)
-            //we zullen points nodig hebben
+            //we zullen points nodig hebben --> array met de punten die we nodig hebben
             Point[] points = new Point[3];
+            points[0] = new Point(200, 100);
+            points[1] = new Point(160, 175);
+            points[2] = new Point(250, 175);
+
+            g.DrawPolygon(redPen, points);
+
+            //trapezuim --> poligon
+            Point[] points2 = new Point[4];
+            points2[0] = new Point(200, 100);
+            points2[1] = new Point(250, 100);
+            points2[2] = new Point(300, 200);
+            points2[3] = new Point(250, 200);
+
+            //nieuwe groene pen
+            SolidBrush greenBrush = new SolidBrush(Color.Green);
+            Pen greenPen = new Pen(greenBrush, 1);
+
+            g.DrawPolygon(greenPen, points2);
+
+            //diabolo van lijnen (bijzonder)
+            Point[] points3 = new Point[2];
+            points3[0] = new Point(10, 10);
+            points3[1] = new Point(300, 300);
+            for (int i = 0; i < 110; i++)
+            {
+                g.DrawLine(greenPen, points3[1], points3[0]);
+                points3[0].X += 3;
+                points3[1].X -= 3;
+            }
+
         }
     }
 }
